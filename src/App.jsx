e7d514a77c072a13dad1e332d52b318e485ca386
SudiasDikko <sudaisumardikko@.com>
2025-02-26 00:58:52 +0100

    personal

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import ContactUs from './pages/ContactUs'
 import AsthmaticStudent from './pages/AsthmaticStudent'
 import Volunteer from './pages/Volunteer'
 import Donate from './pages/Donate'
+import PersonalInfo from './components/PersonalInfo'
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path='' element={<Home/>}/>
+        <Route path='Home' element={<Home/>}/>
         <Route path='about-us' element={<AboutUs/>}/>
         <Route path='blog' element={<Blog/>}/>
         <Route path='contact-us' element={<ContactUs/>}/>
--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import "../style/PersonalInfo.css"
 
 const PersonalInfo = () => {
   return (
-    <div>PersonalInfo</div>
+    <div className='PersonalBody'>
+     <div className='RightDiv'>
+     <div className='ImgDiv'>
+    <img className='Img' src='src/assets/donate-second-pic.png'/>
+     </div>
+     </div>
+     <div className='LeftDiv'>
+      <div className='Wrapper'>
+      <div className='Header'>
+     <h1 className='TextDiv'> Personal Information</h1>
+     <div className='CircleDiv'>
+        <p className='Circle'></p>
+        <p className='Circle'></p>
+        <p className='Circle'></p>
+     </div>
+      </div>
+      <div className='Body'>
+     <div className='InputDiv'>Your full Name
+        <input className='InputStyle' type="Text" placeholder='Type here' />
+     </div>
+     <div className='InputDiv'>Your Phone Number
+        <input className='InputStyle' type="Text" placeholder='Type here' />
+     </div>
+     <div className='InputDiv'>Address
+        <input className='InputStyle' type="Text" placeholder='Type here' />
+     </div>
+     <div className='InputDiv'>Additional Tnformation
+        <input className='InputStyle' type="Text" placeholder='Type here' />
+     </div>
+    <div className='ButtonDiv'>
+    <div className='Button'>Next</div>
+    </div>
+   
+
+      </div>
+
+      </div>
+     </div>
+
+    </div>
   )
 }
 
