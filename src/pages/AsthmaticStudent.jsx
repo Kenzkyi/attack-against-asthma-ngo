0d5f25e60feddf3572ffed27366108471694dbab
Amos-Igbayilolaoluwa-Yentonyon <amosyentonyon@gmail.com>
2025-02-25 16:20:48 +0100

    first commit

--- src/pages/AsthmaticStudent.jsx
+++ src/pages/AsthmaticStudent.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "../style/AsthmaticStudent.css"
+import arrow from "../assets/arrow.png"
+
 
 const AsthmaticStudent = () => {
   return (
-    <div>
-      
+    <div className="bodyCon" >
+      <div className='heroPart'>
+
+        <div className='leftContainer'>
+          <div className='arrowCon'>
+            <img src={arrow} alt="" />
+          </div>
+          
+        </div>
+        <div className='rightContainer'></div>
+
+      </div>
     </div>
   )
 }
