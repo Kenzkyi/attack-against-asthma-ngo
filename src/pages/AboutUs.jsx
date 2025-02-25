0dd40b2d9b19d7459ec6a22d390741aae1537820
Your Name <you@example.com>
2025-02-25 20:24:58 +0100

    My Task

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,85 @@
-import React from 'react'
+import React from "react";
+import "./AboutUs.css";
+import HeroVideo from "../assets/HeroVideo.png";
 
 const AboutUs = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="main-container">
+      <header className="about-grid">
+        <div className="about-left">
+          <div className="divider-cont">
+            <hr className="divider" />
+            <h3 className="subtitle">Know More About Us</h3>
+          </div>
+          <div className="tittle-sec">
+            <h2 className="title">
+              We are a <span className="highlight">non governmental </span>
+              <br />
+              <span className="newline">organisation</span>
+            </h2>
+          </div>
+        </div>
+        <div className="about-right">
+          <h3 className="mission-tittle">
+            Empowering Lives, One Breath at a Time
+          </h3>
+          <p className="mission-text">
+            At Attack Against Asthma, we believe that no one should have to
+            strugle for a breath. Our mission is to fight back against asthma
+            through education, advocacy, and direct support to those who need it
+            most-especially students.
+          </p>
+        </div>
+      </header>
+      <div className="image-container">
+        <img
+          src={HeroVideo}
+          alt="Team members of Attack Against Asthma"
+          className="team-image"
+        />
 
-export default AboutUs
+        <button className="play-button">▶</button>
+      </div>
+      <div className="bio-mission">
+        <h2 className="bio-title">Brif Bio, Mission & Vision</h2>
+        <p className="bio-founder">
+          Founded by Temitope Omosebi Enoch and a passionate group <br /> of
+          healthcare professionals, educators, and community leaders
+        </p>
+        <p className="bio-text">
+          At Attack Against Asthma, we believe that no one should have to
+          struggle for a breath. Our mission is to fight back against asthma
+          through education, <br />
+          advocacy, and direct support to those who need it most, especially
+          students. We are dedicated to raising awareness, providing resources,
+          and creating safer <br />
+          environments for individuals living with asthma.
+        </p>
+        <p className="bio-text">
+          Founded by a passionate group of healthcare professionals, educators,
+          and community leaders, we understand the daily challenges that asthma
+          can bring, <br />
+          particularly for young students. Through our targeted programs,
+          including inhaler distribution, workshops, and awareness campaigns, we
+          equip both <br />
+          students and schools with the knowledge and tools to manage asthma
+          effectively.
+        </p>
+        <p className="bio-text">
+          We work tirelessly to raise awareness about asthma's impact on
+          students' lives, ensuring that teachers, parents, and peers are
+          informed and ready to support. <br />
+          Our asthma-friendly initiatives, like providing inhalers to schools
+          and organizing community outreach programs, help create safer and
+          healthier spaces for all <br />
+          students.
+        </p>
+      </div>
+      <div className="divider-cont">
+        <hr className="second-divider" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
