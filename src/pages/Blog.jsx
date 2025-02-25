fd8749da75a08a2e50351d7edc8c4e0b8a7572ce
Tersemajesty <okekechidera992@gmail.com>
2025-02-25 14:53:09 -0800

    collabs commit

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,183 @@
-import React from 'react'
+
+import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>
-               
+   <div className='Body'>
+    <div className='bodywrap'>
+         <div className='textwrap'>
+             <p>
+              Know about attack against asthma
+             </p>
+             <h1>
+              We are non governmental oraganisation
+             </h1>
+         </div>
+         <div className='imagewrap'>
+          <div className='imagewrap1'>
+            <div className='img'>
+            <img src="src/assets/home-fourth-pic.png" alt="" />
+            </div>
+            <div className='textt'>
+            <h1>
+              #AttackAgainstAsthma in the university of ibadan
+            </h1>
+            </div>
+            <div className='text1'>
+             <h1>
+              on the 22nd of september 2023, Temitope Omosebi initiated a aheslth charity Program 
+              tagged AttackAggainstAsthema by donating
+              50 inhalers to asthemati student in the University of Ibadan 
+              The team that carried the program included the above named individual and some 
+              of our <strong>VOLUNTEERS....</strong>
+             </h1>
+            </div>
+            <div className='btn'>
+                <button>Learn more</button>
+            </div>
+          </div>
+          <div className='imagewrap3'>
+          <div className='img'>
+            <img src="src/assets/home-fourth-pic.png" alt="" />
+            </div>
+            <div className='textt'>
+            <h1>
+              #AttackAgainstAsthma in the university of ibadan
+            </h1>
+            </div>
+            <div className='text1'>
+             <h1>
+              on the 22nd of september 2023, Temitope Omosebi initiated a aheslth charity Program 
+              tagged AttackAggainstAsthema by donating
+              50 inhalers to asthemati student in the University of Ibadan 
+              The team that carried the program included the above named individual and some 
+              of our <strong>VOLUNTEERS....</strong>
+             </h1>
+            </div>
+            <div className='btn'>
+                <button>Learn more</button>
+            </div>
+          </div>
+          <div className='imagewrap2'>
+          <div className='img'>
+            <img src="src/assets/home-fourth-pic.png" alt="" />
+            </div>
+            <div className='textt'>
+            <h1>
+              #AttackAgainstAsthma in the university of ibadan
+            </h1>
+            </div>
+            <div className='text1'>
+             <h1>
+              on the 22nd of september 2023, Temitope Omosebi initiated a aheslth charity Program 
+              tagged AttackAggainstAsthema by donating
+              50 inhalers to asthemati student in the University of Ibadan 
+              The team that carried the program included the above named individual and some 
+              of our <strong>VOLUNTEERS.....</strong>
+             </h1>
+            </div>
+            <div className='btn'>
+                <button>Learn more</button>
+            </div>
+          </div>
+         </div>
     </div>
+    <div className='bodywrap1'>
+    <div className='imagewrap'>
+          <div className='imagewrap1'>
+            <div className='img'>
+            <img src="src/assets/home-fourth-pic.png" alt="" />
+            </div>
+            <div className='textt'>
+            <h1>
+              #AttackAgainstAsthma in the university of ibadan
+            </h1>
+            </div>
+            <div className='text1'>
+             <h1>
+              on the 22nd of september 2023, Temitope Omosebi initiated a aheslth charity Program 
+              tagged AttackAggainstAsthema by donating
+              50 inhalers to asthemati student in the University of Ibadan 
+              The team that carried the program included the above named individual and some 
+              of our <strong>VOLUNTEERS,.....</strong>
+             </h1>
+            </div>
+            <div className='btn'>
+                <button>Learn more</button>
+            </div>
+          </div>
+
+          <div className='imagewrap3'>
+          <div className='img'>
+            <img src="src/assets/home-fourth-pic.png" alt="" />
+            </div>
+            <div className='textt'>
+            <h1>
+              #AttackAgainstAsthma in the university of ibadan
+            </h1>
+            </div>
+            <div className='text1'>
+             <h1>
+              on the 22nd of september 2023, Temitope Omosebi initiated a aheslth charity Program 
+              tagged AttackAggainstAsthema by donating
+              50 inhalers to asthemati student in the University of Ibadan 
+              The team that carried the program included the above named individual and some 
+              of our <strong>VOLUNTEERS......</strong>
+             </h1>
+            </div>
+            <div className='btn'>
+                <button>Learn more</button>
+            </div>
+          </div>
+
+          <div className='imagewrap2'>
+          <div className='img'>
+            <img src="src/assets/home-fourth-pic.png" alt="" />
+            </div>
+            <div className='textt'>
+            <h1>
+              #AttackAgainstAsthma in the university of ibadan
+            </h1>
+            </div>
+            <div className='text1'>
+             <h1>
+              on the 22nd of september 2023, Temitope Omosebi initiated a aheslth charity Program 
+              tagged AttackAggainstAsthema by donating
+              50 inhalers to asthemati student in the University of Ibadan 
+              The team that carried the program included the above named individual and some 
+              of our <strong>VOLUNTEERS......</strong>
+             </h1>
+            </div>
+            <div className='btn'>
+                <button>Learn more</button>
+            </div>
+          </div>
+         </div>
+</div>
+     <div className='bodywrap2'>
+     <div className='bodywrapper' >
+    <div className='bodywrapper2'>
+      <p>Health Tips</p>
+      <h1>Herere 10 Things To Know As An <strong>Asthmatics</strong></h1>
+      <h2>navigating with certain health conditiond might
+        seems impossible and biarsed but we have gathered sreamlined information
+        and tips for you to effectively scale through............ 
+
+      </h2>
+      <button>Learn more</button>
+    </div>
+     </div>
+     <div className='bodywrapper1' >
+  <div className='manshowing'>
+<img src="src/assets/blog-second-pic.png" alt="" />
+  </div>
+     </div>
+     </div>
+
+   </div>
+   
   )
 }
 
 export default Blog
+
