86d4e74d7bfcd477a53c051998ec175d8ce912de
Kenz <ekeneokoye93@gmail.com>
2025-02-25 15:27:06 +0100

    asthma-ngo

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import Blog from './pages/Blog'
+import ContactUs from './pages/ContactUs'
+import AsthmaticStudent from './pages/AsthmaticStudent'
+import Volunteer from './pages/Volunteer'
+import Donate from './pages/Donate'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='about-us' element={<AboutUs/>}/>
+        <Route path='blog' element={<Blog/>}/>
+        <Route path='contact-us' element={<ContactUs/>}/>
+        <Route path='asthmatic-student' element={<AsthmaticStudent/>}/>
+        <Route path='volunteer' element={<Volunteer/>}/>
+        <Route path='donate' element={<Donate/>}/>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
---
+++ src/pages/AsthmaticStudent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AsthmaticStudent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AsthmaticStudent
