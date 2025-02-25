959a21ff390ce6cc28e4b7a6826890b4c9bd2c33
Kenz <ekeneokoye93@gmail.com>
2025-02-25 17:05:28 +0100

    contact-us

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const ContactUs = () => {
   return (
-    <div>
-      
+    <div className='contact-us'>
+      <Header/>
+      <div className="contact-us-wrapper"></div>
+      <Footer/>
     </div>
   )
 }
