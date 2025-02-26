d2a9de5508af5d92b0bd64806b52f89f5bf3b10d
SudiasDikko <sudaisumardikko@.com>
2025-02-26 22:12:50 +0100

    personal

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import ContactUs from './pages/ContactUs'
 import AsthmaticStudent from './pages/AsthmaticStudent'
 import Volunteer from './pages/Volunteer'
 import Donate from './pages/Donate'
-import SelectAmount from './components/SelectAmount'
+
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path='' element={<SelectAmount/>}/>
+        <Route path='' element={<Home/>}/>
         <Route path='about-us' element={<AboutUs/>}/>
         <Route path='blog' element={<Blog/>}/>
         <Route path='contact-us' element={<ContactUs/>}/>
