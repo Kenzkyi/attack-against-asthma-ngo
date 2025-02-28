f3d1b78a5a85fbc60603b8c8f48274a0129f8fd8
Tersemajesty <okekechidera992@gmail.com>
2025-02-28 00:59:22 -0800

    recent update

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import "../style/donate.css"
-=======
-import React from 'react'
 import "../style/Donate.css"
->>>>>>> 33554b8ba99b183bdb833139adbe84207593b0ab
 const Donate = () => {
   return (
     <div>
@@ -11,10 +6,10 @@ const Donate = () => {
       <div className="hero-overlay"></div>
       <div className="hero-content">
         <span className="donate-label">Donate to a Student</span>
-        <h1 className="hero-title">"Only By Helping Each Other Can We Make The World A Better Place"</h1>
+        <h1 className="hero-title">Only By Helping Each Other Can We Make The World A Better Place</h1>
         <p className="hero-quote">
-          "Donating is not just about giving; its about creating a ripple of kindness that touches lives and inspires
-          others to do the same."
+          Donating is not just about giving; its about creating a ripple of kindness that touches lives and inspires
+          others to do the same.
         </p>
       </div>
     </div>
